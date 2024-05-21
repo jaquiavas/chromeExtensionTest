28edd3ec3efcc654314075e3005dd38f05b0f65c
schr1841 <schr1841@d.umn.edu>
2024-05-21 10:26:21 -0500

    messing with some permissions and tab overrrides

--- contentScript.js
+++ contentScript.js
@@ -1,3 +1,11 @@
 chrome.runtime.sendMessage({greeting: "hello"}, function(response){
     console.log(response.farewell);
 })
+
+chrome.storage.local.set({ name: 'xander' }).then(() => {
+    console.log("Value is set");
+  });
+  
+  chrome.storage.local.get(["name"]).then((result) => {
+    console.log("Value is " + result.name);
+  });
